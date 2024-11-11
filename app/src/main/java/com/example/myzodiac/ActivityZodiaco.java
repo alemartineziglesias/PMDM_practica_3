492a14eb422909fbf8a9783715cfad42758d8f49
alemartineziglesias <alemartineziglesias427@gmail.com>
2024-11-11 18:08:59 +0100

    Funcionalidad

--- app/src/main/java/com/example/myzodiac/ActivityZodiaco.java
+++ app/src/main/java/com/example/myzodiac/ActivityZodiaco.java
@@ -1,6 +1,7 @@
 package com.example.myzodiac;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +9,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ActivityZodiaco extends AppCompatActivity {
+public class ActivityZodiaco extends AppCompatActivity
+{
+    TextView resultadosEdad;
+    TextView resultadosZodiaco;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_zodiaco);
@@ -20,5 +25,22 @@ public class ActivityZodiaco extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        resultadosEdad = findViewById(R.id.textViewEdad);
+        resultadosZodiaco = findViewById(R.id.textViewZodiaco);
+        Bundle extras = getIntent().getExtras();
+        int edad = extras.getInt("edad");
+        String signo = extras.getString("signo");
+        if(edad == 1)
+        {
+            String stringEdad = getString(R.string.resultados_edad) + " " + edad + " " + getString(R.string.fin_edad_alterno);
+            resultadosEdad.setText(stringEdad);
+        }
+        else
+        {
+            String stringEdad = getString(R.string.resultados_edad) + " " + edad + " " + getString(R.string.fin_edad);
+            resultadosEdad.setText(stringEdad);
+        }
+        String stringZodiaco = getString(R.string.resultados_zodiaco) + " " + signo;
+        resultadosZodiaco.setText(stringZodiaco);
     }
 }
--- app/src/main/java/com/example/myzodiac/MainActivity.java
+++ app/src/main/java/com/example/myzodiac/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myzodiac;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
 
@@ -15,13 +18,19 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
     CalendarView calendario;
     TextView fecha;
+    Button aceptar;
+    String fechaElegida;
+    Calendar calendar;
+    SimpleDateFormat dateFormat;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -32,18 +41,98 @@ public class MainActivity extends AppCompatActivity
         });
         calendario = findViewById(R.id.calendarView);
         fecha = findViewById(R.id.textViewFecha);
-        Calendar calendar = Calendar.getInstance();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
-        String fechaActual = dateFormat.format(calendar.getTime());
-        fecha.setText(fechaActual);
+        aceptar = findViewById(R.id.buttonAceptar);
+        calendar = Calendar.getInstance();
+        dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        fechaElegida = dateFormat.format(calendar.getTime());
+        fecha.setText(fechaElegida);
         calendario.setOnDateChangeListener(new CalendarView.OnDateChangeListener()
         {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int anio, int mes, int dia)
             {
-                String fechaElegida = dia + "/" + (mes + 1) + "/" + anio;
+                fechaElegida = dia + "/" + (mes + 1) + "/" + anio;
                 fecha.setText(fechaElegida);
             }
         });
+        aceptar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, ActivityZodiaco.class);
+        String[] fechaCortada = fechaElegida.split("/");
+        int dia = Integer.parseInt(fechaCortada[0]);
+        int mes = Integer.parseInt(fechaCortada[1]);
+        int anio = Integer.parseInt(fechaCortada[2]);
+        String fechaActual = dateFormat.format(calendar.getTime());
+        String[] fechaActualCortada = fechaActual.split("/");
+        int diaActual = Integer.parseInt(fechaActualCortada[0]);
+        int mesActual = Integer.parseInt(fechaActualCortada[1]);
+        int anioActual = Integer.parseInt(fechaActualCortada[2]);
+        int edad = anioActual - anio;
+        if(diaActual < dia && mesActual == mes || diaActual >= dia && mesActual < mes || diaActual < dia && mesActual < mes)
+        {
+            edad = edad - 1;
+        }
+        if(edad < 0)
+        {
+            String stringError = getString(R.string.error);
+            fecha.setText(stringError);
+        }
+        else
+        {
+            intent.putExtra("edad", edad);
+            if(dia >= 21 && mes == 3 || dia <= 19 && mes == 4)
+            {
+                intent.putExtra("signo", getString(R.string.aries));
+            }
+            else if(dia >= 20 && mes == 4 || dia <= 20 && mes == 5)
+            {
+                intent.putExtra("signo", getString(R.string.tauro));
+            }
+            else if(dia >= 21 && mes == 5 || dia <= 20 && mes == 6)
+            {
+                intent.putExtra("signo", getString(R.string.geminis));
+            }
+            else if(dia >= 23 && mes == 6 || dia <= 22 && mes == 7)
+            {
+                intent.putExtra("signo", getString(R.string.cancer));
+            }
+            else if(dia >= 23 && mes == 7 || dia <= 22 && mes == 8)
+            {
+                intent.putExtra("signo", getString(R.string.leo));
+            }
+            else if(dia >= 23 && mes == 8 || dia <= 22 && mes == 9)
+            {
+                intent.putExtra("signo", getString(R.string.virgo));
+            }
+            else if(dia >= 23 && mes == 9 || dia <= 22 && mes == 10)
+            {
+                intent.putExtra("signo", getString(R.string.libra));
+            }
+            else if(dia >= 23 && mes == 10 || dia <= 21 && mes == 11)
+            {
+                intent.putExtra("signo", getString(R.string.escorpio));
+            }
+            else if(dia >= 22 && mes == 11 || dia <= 21 && mes == 12)
+            {
+                intent.putExtra("signo", getString(R.string.sagitario));
+            }
+            else if(dia >= 22 && mes == 12 || dia <= 19 && mes == 1)
+            {
+                intent.putExtra("signo", getString(R.string.capricornio));
+            }
+            else if(dia >= 20 && mes == 1 || dia <= 18 && mes == 2)
+            {
+                intent.putExtra("signo", getString(R.string.acuario));
+            }
+            else
+            {
+                intent.putExtra("signo", getString(R.string.piscis));
+            }
+            startActivity(intent);
+        }
     }
 }
